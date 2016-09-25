72bdea37ae7c202a5b13872d179cf18529747255
aeroson <theaeroson@gmail.com>
2016-09-25 22:07:24 +0000

    added -noLogs for headless clients, dont want them spamming server rpt log

--- app/views/backend/server/cfg_hc2_xml.blade.php
+++ app/views/backend/server/cfg_hc2_xml.blade.php
@@ -6,7 +6,7 @@
   <DisplayNamePrefix>ArmA 3 - </DisplayNamePrefix>
   <WorkingDir>C:\arma3</WorkingDir>
   <Executable>C:\arma3\arma3server.exe</Executable>
-  <Parameters>-client -connect=127.0.0.1 -password={{ $server->private_password }} -name=arma3 -profiles=C:\arma3\instances\{{ $server->name }}\profile -cfg=C:\arma3\instances\{{ $server->name }}\basic.cfg -par=C:\arma3\instances\{{ $server->name }}\parameters.cfg -port={{ $server->port }}2</Parameters>
+  <Parameters>-client -noLogs -connect=127.0.0.1 -password={{ $server->private_password }} -name=arma3 -profiles=C:\arma3\instances\{{ $server->name }}\profile -cfg=C:\arma3\instances\{{ $server->name }}\basic.cfg -par=C:\arma3\instances\{{ $server->name }}\parameters.cfg -port={{ $server->port }}2</Parameters>
   <Delay>3000</Delay>
   <StartUpMode>0</StartUpMode>
   <ForceReplace>true</ForceReplace>
