1606c56682272bc149519a9bde3c8ae29d6f2489
Flogard <trym2001@hotmail.com>
2016-11-16 14:18:06 +0100

    debug

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -37,7 +37,9 @@ class UserController extends BaseController
 		}
 		$user->save();
 		
-		return Redirect::to('backend#backend/my-profile');
+		print_r(Input::file('picture'));
+		
+		//return Redirect::to('backend#backend/my-profile');
     }
 
     public function GetCreate()
