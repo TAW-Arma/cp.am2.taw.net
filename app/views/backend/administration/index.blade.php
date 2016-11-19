8ddb2d5e6eb3a1eb78c448af13254a72313a1533
Flogard <trym2001@hotmail.com>
2016-11-19 23:01:03 +0100

    no message

--- app/views/backend/administration/index.blade.php
+++ app/views/backend/administration/index.blade.php
@@ -3,23 +3,61 @@
         <article class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
             <div class="jarviswidget" id="wid-id-1" data-widget-colorbutton="false" data-widget-editbutton="false" data-widget-togglebutton="false" data-widget-deletebutton="false" data-widget-fullscreenbutton="false" data-widget-custombutton="false" data-widget-collapsed="false" data-widget-sortable="false">
                 <header>
-                    <span class="widget-icon"> <i class="fa fa-id-badge"></i> </span>
-                    <h2>{{ Lang::get('administration.administration') }}</h2>
+                    <span class="widget-icon"> <i class="fa fa-table"></i> </span>
+                    <h2>{{ Lang::get('dashboard.cpu') }}</h2>
                 </header>
                 <div>
                     <div class="jarviswidget-editbox">
                     </div>
                     <div class="widget-body no-padding">
-                        <fieldset>
-                                <div class="row">
-                                    <section class="col col-12">
-                                        <label for="updatearma">
-                                            {{ Lang::get('users.updatearma') }}
-                                        </label>
-                                        <div class="button"></i>{{ Lang::get('users.update') }}</div>
-                                    </section>
-                                </div>
-                            </fieldset>
+                        <img class="img-responsive" src="https://observium.jaapjolman.nl/graph.php?type=device_processor&height=250&width=700&legend=no&device=308" />
+                    </div>
+                </div>
+            </div>
+        </article>
+        <article class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+            <div class="jarviswidget" id="wid-id-1" data-widget-colorbutton="false" data-widget-editbutton="false" data-widget-togglebutton="false" data-widget-deletebutton="false" data-widget-fullscreenbutton="false" data-widget-custombutton="false" data-widget-collapsed="false" data-widget-sortable="false">
+                <header>
+                    <span class="widget-icon"> <i class="fa fa-exchange"></i> </span>
+                    <h2>{{ Lang::get('dashboard.network') }}</h2>
+                </header>
+                <div>
+                    <div class="jarviswidget-editbox">
+                    </div>
+                    <div class="widget-body no-padding">
+                        <img class="img-responsive" src="https://observium.jaapjolman.nl/graph.php?type=device_bits&height=250&width=700&legend=no&device=308" />
+                    </div>
+                </div>
+            </div>
+        </article>
+    </div>
+    <div class="row">
+        <article class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+            <div class="jarviswidget" id="wid-id-1" data-widget-colorbutton="false" data-widget-editbutton="false" data-widget-togglebutton="false" data-widget-deletebutton="false" data-widget-fullscreenbutton="false" data-widget-custombutton="false" data-widget-collapsed="false" data-widget-sortable="false">
+                <header>
+                    <span class="widget-icon"> <i class="fa fa-table"></i> </span>
+                    <h2>{{ Lang::get('dashboard.memory') }}</h2>
+                </header>
+                <div>
+                    <div class="jarviswidget-editbox">
+                    </div>
+                    <div class="widget-body no-padding">
+                        <img class="img-responsive" src="https://observium.jaapjolman.nl/graph.php?type=device_mempool&height=250&width=700&legend=yes&device=308"/>
+                    </div>
+                </div>
+            </div>
+        </article>
+        <article class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+            <div class="jarviswidget" id="wid-id-1" data-widget-colorbutton="false" data-widget-editbutton="false" data-widget-togglebutton="false" data-widget-deletebutton="false" data-widget-fullscreenbutton="false" data-widget-custombutton="false" data-widget-collapsed="false" data-widget-sortable="false">
+                <header>
+                    <span class="widget-icon"> <i class="fa fa-hdd-o"></i> </span>
+                    <h2>{{ Lang::get('dashboard.filesystem') }}</h2>
+                </header>
+                <div>
+                    <div class="jarviswidget-editbox">
+                    </div>
+                    <div class="widget-body no-padding">
+                        <img class="img-responsive" src="https://observium.jaapjolman.nl/graph.php?type=device_storage&height=250&width=700&legend=yes&device=308" />
                     </div>
                 </div>
             </div>
