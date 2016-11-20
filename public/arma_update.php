f5cfa2e6f4cf0cad2f93911f8dfc071d1a55126a
Flogard <trym2001@hotmail.com>
2016-11-20 13:05:53 +0100

    new way

--- public/arma_update.php
+++ public/arma_update.php
@@ -1,27 +1,19 @@
 <?php
 
-	header('Content-Encoding: none;');
+	$cmd = "C:/steamcmd/steamcmd.exe +login anonymous +force_install_dir \"C:\Steam\steamapps\common\Arma 3 Server\" +app_update 233780 validate +quit";
 
-	set_time_limit(0);
-
-	$handle = popen("C:/steamcmd/steamcmd.exe +login anonymous +force_install_dir \"C:\Steam\steamapps\common\Arma 3 Server\" +app_update 233780 validate +quit", "r");
-
-	if (ob_get_level() == 0) 
-		ob_start();
-
-	while(!feof($handle)) {
-
-		$buffer = fgets($handle);
-		$buffer = trim(htmlspecialchars($buffer));
-
-		echo $buffer . "<br />";
-		echo str_pad('', 4096);    
-
-		ob_flush();
-		flush();
-		sleep(1);
+	$descriptorspec = array(
+	   0 => array("pipe", "r"),   // stdin is a pipe that the child will read from
+	   1 => array("pipe", "w"),   // stdout is a pipe that the child will write to
+	   2 => array("pipe", "w")    // stderr is a pipe that the child will write to
+	);
+	flush();
+	$process = proc_open($cmd, $descriptorspec, $pipes, realpath('./'), array());
+	if (is_resource($process)) {
+		while ($s = fgets($pipes[1])) {
+			print $s;
+			flush();
+		}
 	}
 
-	pclose($handle);
-	ob_end_flush();
 ?>
