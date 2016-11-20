53614722c313ff0144d1a85402e9a5f79c242923
Flogard <trym2001@hotmail.com>
2016-11-20 13:00:06 +0100

    removed ajax

--- app/views/backend/administration/index.blade.php
+++ app/views/backend/administration/index.blade.php
@@ -13,7 +13,32 @@
                     <div class="jarviswidget-editbox">
                     </div>
 					<div class="widget-body no-padding">
-                        <div id="console_output"></div>
+                        <div id="console_output">
+						<?php
+							set_time_limit(0);
+
+							$handle = popen("C:/steamcmd/steamcmd.exe +login anonymous +force_install_dir \"C:\Steam\steamapps\common\Arma 3 Server\" +app_update 233780 validate +quit", "r");
+
+							if (ob_get_level() == 0) 
+								ob_start();
+
+							while(!feof($handle)) {
+
+								$buffer = fgets($handle);
+								$buffer = trim(htmlspecialchars($buffer));
+
+								echo $buffer . "<br />";
+								echo str_pad('', 4096);    
+
+								ob_flush();
+								flush();
+								sleep(1);
+							}
+
+							pclose($handle);
+							ob_end_flush();
+						?>
+						</div>
                     </div>
                 </div>
             </div>
