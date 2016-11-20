a945b82f8ab5fe0f3429b2fc8cd7ec6871a24e68
Flogard <trym2001@hotmail.com>
2016-11-20 13:56:00 +0100

    view file updates

--- app/views/backend/administration/index.blade.php
+++ app/views/backend/administration/index.blade.php
@@ -3,10 +3,10 @@
         <article class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
             <div class="jarviswidget" id="wid-id-1" data-widget-colorbutton="false" data-widget-editbutton="false" data-widget-togglebutton="false" data-widget-deletebutton="false" data-widget-fullscreenbutton="false" data-widget-custombutton="false" data-widget-collapsed="false" data-widget-sortable="false">
                 <header>
-                    <span class="widget-icon"> <i class="fa fa-table"></i> </span>
+                    <span class="widget-icon"> <i class="fa fa-cloud-download"></i> </span>
                     <h2>{{ Lang::get('administration.updatearma') }}</h2>
                     <div class="widget-toolbar" role="menu">
-                        <button id="update_arma" class="btn btn-default">{{ Lang::get('general.back') }}</button>
+                        <button id="update_arma" class="btn btn-default">{{ Lang::get('administration.update') }}</button>
                     </div>
                 </header>
                 <div>
